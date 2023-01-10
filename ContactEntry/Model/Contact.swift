bc56b37c8e891ee09a467ab6208b88187d75775c
Stainley Lebron <saiyamans@gmail.com>
2023-01-10 09:06:46 -0500

    Contact model

---
+++ ContactEntry/Model/Contact.swift
@@ -0,0 +1,43 @@
+//
+//  Contact.swift
+//  ContactEntry
+//
+//  Created by Stainley A Lebron R on 2023-01-10.
+//
+
+import Foundation
+
+struct Contact {
+    
+    private var firstName: String
+    private var lastName: String
+    private var phoneNumber: String?
+    private var email: String?
+    
+    init(firstName: String, lastName: String, phoneNumber: String? = nil, email: String? = nil) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.phoneNumber = phoneNumber
+        self.email = email
+    }
+      
+    
+    func getFirstName() -> String {
+        return firstName;
+    }
+    
+    func getLastName() -> String {
+        return lastName;
+    }
+    
+    func getPhoneNumber() -> String? {
+        if phoneNumber != nil {
+            return phoneNumber
+        }
+        return nil
+    }
+    
+    func getEmail() -> String? {
+        return email != nil ? email : nil
+    }
+}
