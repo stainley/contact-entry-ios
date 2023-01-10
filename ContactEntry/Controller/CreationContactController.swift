d30350d5ce123eecc4df64344e6b759a75818a4d
Stainley Lebron <saiyamans@gmail.com>
2023-01-10 08:29:06 -0500

    Creation Contact binding

---
+++ ContactEntry/Controller/CreationContactController.swift
@@ -0,0 +1,15 @@
+//
+//  CreationContactController.swift
+//  ContactEntry
+//
+//  Created by Stainley A Lebron R on 2023-01-10.
+//
+
+import UIKit
+
+class CreationContactController : UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
