336bef36a941a94c29410c6c7edc787520aeba2d
Stainley Lebron <saiyamans@gmail.com>
2023-01-10 08:26:50 -0500

    Binding contact detail

---
+++ ContactEntry/Controller/ContactDetailController.swift
@@ -0,0 +1,15 @@
+//
+//  ContactDetailController.swift
+//  ContactEntry
+//
+//  Created by Stainley A Lebron R on 2023-01-10.
+//
+
+import UIKit
+
+class ContactDetailController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
